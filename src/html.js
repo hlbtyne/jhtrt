51f67dffa2259b8c776bfe47a5752ec40a175867
Harriet Ballantyne <harriet.ballantyne@gmail.com>
2020-06-18 11:17:49 +0100

    jh: navbar fix

--- src/html.js
+++ src/html.js
@@ -24,7 +24,7 @@ export default function HTML(props) {
         />
         <script src="https://code.jquery.com/jquery-git2.min.js"></script>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-        <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>
       </head>
       <body {...props.bodyAttributes}>
         {props.preBodyComponents}
