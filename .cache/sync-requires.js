2d04a50299a8a8f8f9bf84e4e303a7a40746b67e
Harriet Ballantyne <harriet.ballantyne@gmail.com>
2020-11-11 11:51:33 +0000

    jh: deploy

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/harrietballantyne/Dev/jhtrt/jhtrt-site/.cache/dev-404-page.js"))),
   "component---node-modules-gatsby-source-prismic-graphql-components-preview-page-js": hot(preferDefault(require("/Users/harrietballantyne/Dev/jhtrt/jhtrt-site/node_modules/gatsby-source-prismic-graphql/components/PreviewPage.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/harrietballantyne/Dev/jhtrt/jhtrt-site/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/harrietballantyne/Dev/jhtrt/jhtrt-site/src/pages/contact.js"))),
