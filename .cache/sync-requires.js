5a92500cd1c13129d391e6f4b3b49c802673f8ef
Harriet Ballantyne <harriet.ballantyne@pollen.co>
2021-04-25 17:06:49 +0100

    videos

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => m && m.default || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/.cache/dev-404-page.js"))),
   "component---node-modules-gatsby-source-prismic-graphql-components-preview-page-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/node_modules/gatsby-source-prismic-graphql/components/PreviewPage.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/src/pages/contact.js"))),
