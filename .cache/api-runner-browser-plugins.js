23908adab741e498e464ed58c3bddf76535efa91
Harriet Ballantyne <harriet.ballantyne@pollen.co>
2022-02-17 15:03:07 -0800

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-source-prismic-graphql/gatsby-browser.js'),
-      options: {"plugins":[],"repositoryName":"jhtrt","langs":["en-gb"],"defaultLang":"en-gb","previews":true},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
