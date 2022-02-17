a24a358473a5556346ebb9dea371b48e8a5c72ef
Harriet Ballantyne <harriet.ballantyne@pollen.co>
2022-02-17 11:00:56 -0800

    jh: donald obitruary, new covid projects, copy additions

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,24 +1,36 @@
-import React from "react";
-import "bootswatch/dist/lux/bootstrap.min.css";
-import logo from '../../images/logo_icon.png';
-import './Navbar.css';
+import React from "react"
+import "bootswatch/dist/lux/bootstrap.min.css"
+import logo from "../../images/logo_icon.png"
+import "./Navbar.css"
 
-import Link from "gatsby-link";
+import Link from "gatsby-link"
 
 export const Navbar = () => (
-  <nav class="navbar navbar-expand-lg navbar-light bg-light" style={{ "zIndex": 5, "position": "sticky", "top": 0, "padding": "5px 10px"}}>
-    <a class="navbar-brand" href="/#"><Link to="/"><img src={logo} style={{ "height": "60px" }}/></Link></a>
-    <button 
-      class="navbar-toggler toggler-example" 
-      type="button" 
-      data-toggle="collapse" 
-      data-target="#navbarColor03" 
-      aria-controls="navbarColor03" 
-      aria-expanded="false" 
-      aria-label="Toggle navigation">
+  <nav
+    class="navbar navbar-expand-lg navbar-light bg-light"
+    style={{ zIndex: 5, position: "sticky", top: 0, padding: "5px 10px" }}
+  >
+    <a class="navbar-brand" href="/#">
+      <Link to="/">
+        <img src={logo} alt="logo" style={{ height: "60px" }} />
+      </Link>
+    </a>
+    <button
+      class="navbar-toggler toggler-example"
+      type="button"
+      data-toggle="collapse"
+      data-target="#navbarColor03"
+      aria-controls="navbarColor03"
+      aria-expanded="false"
+      aria-label="Toggle navigation"
+    >
       <span class="navbar-toggler-icon"></span>
     </button>
-    <div class="navbar-collapse collapse" id="navbarColor03" style={{ "padding": "0 10px" }}>
+    <div
+      class="navbar-collapse collapse"
+      id="navbarColor03"
+      style={{ padding: "0 10px" }}
+    >
       <ul class="navbar-nav mr-auto">
         <li class="nav-item">
           <Link to="/about">
@@ -51,6 +63,6 @@ export const Navbar = () => (
       </ul>
     </div>
   </nav>
-);
+)
 
-export default Navbar;
+export default Navbar
