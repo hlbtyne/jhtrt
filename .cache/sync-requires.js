1474060c1f43ecc924637d14e625c99d6b1e2acf
Harriet Ballantyne <harriet.ballantyne@pollen.co>
2022-02-17 14:59:34 -0800

    jh: fix env

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -1,16 +1,17 @@
 const { hot } = require("react-hot-loader/root")
 
 // prefer default export if available
-const preferDefault = m => m && m.default || m
+const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---node-modules-gatsby-source-prismic-graphql-components-preview-page-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/node_modules/gatsby-source-prismic-graphql/components/PreviewPage.js"))),
-  "component---src-pages-about-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/src/pages/about.js"))),
-  "component---src-pages-contact-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/src/pages/contact.js"))),
-  "component---src-pages-index-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/src/pages/index.js"))),
-  "component---src-pages-legacy-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/src/pages/legacy.js"))),
-  "component---src-pages-submissions-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/src/pages/submissions.js"))),
-  "component---src-pages-trustees-js": hot(preferDefault(require("/Users/pollen/Dev/jhtrt/jhtrt/src/pages/trustees.js")))
+  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/.cache/dev-404-page.js"))),
+  "component---node-modules-gatsby-source-prismic-graphql-components-preview-page-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/node_modules/gatsby-source-prismic-graphql/components/PreviewPage.js"))),
+  "component---src-pages-about-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/src/pages/about.js"))),
+  "component---src-pages-contact-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/src/pages/contact.js"))),
+  "component---src-pages-index-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/src/pages/index.js"))),
+  "component---src-pages-legacy-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/src/pages/legacy.js"))),
+  "component---src-pages-submissions-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/src/pages/submissions.js"))),
+  "component---src-pages-trustees-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/src/pages/trustees.js")))
 }
 
