3524544bb7e0fba0917a6d8c59c7298782a1404e
Harriet Ballantyne <harriet.ballantyne@pollen.co>
2024-07-25 13:04:57 -0700

    jh: deploy

--- .cache/sync-requires.js
+++ .cache/sync-requires.js
@@ -5,7 +5,6 @@ const preferDefault = m => (m && m.default) || m
 
 
 exports.components = {
-  "component---cache-dev-404-page-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/.cache/dev-404-page.js"))),
   "component---node-modules-gatsby-source-prismic-graphql-components-preview-page-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/node_modules/gatsby-source-prismic-graphql/components/PreviewPage.js"))),
   "component---src-pages-about-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/src/pages/about.js"))),
   "component---src-pages-contact-js": hot(preferDefault(require("/Users/harriet.ballantyne/Development/jhtrt/src/pages/contact.js"))),
